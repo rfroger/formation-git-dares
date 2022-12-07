3ba88b334d826dda09075bf9c8f40ffd1332b3fa
rfroger <raphael.froger@travail.gouv.fr>
2022-12-07 10:53:43 +0000

    suppression de script1 et changement script2

--- scripts/script1.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-test <- data.frame(c(1:100))
-test2 <- test %>%
-  mutate(a = case_when(
-    c.1.100. < 50 ~ "inferieur à 50",
-    TRUE ~ "50 et +"
-  ))
-
--- scripts/script2.R
+++ scripts/script2.R
@@ -4,5 +4,10 @@ test_v2_2 <- test_v2 %>%
   mutate(a = case_when(
     c.1.1e.07. < 5000 ~ "inferieur à 5000",
     TRUE ~ "50000 et +"
+  )) %>%
+  filter(a == "5000 et +") %>%
+  mutate(b = case_when(
+    c.1.1e.07. < 15000 ~ "inferieur à 15000",
+    TRUE ~ "15000 et +"
   ))
 
