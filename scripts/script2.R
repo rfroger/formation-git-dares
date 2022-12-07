5f8cd5a455be7b656f7ed80d15858340113af44f
rfroger <raphael.froger@travail.gouv.fr>
2022-12-07 13:37:21 +0000

    .txt qui continue d'apparaître

--- scripts/script2.R
+++ scripts/script2.R
@@ -3,7 +3,7 @@ test_v2 <- data.frame(c(1:10000000))
 test_v2_2 <- test_v2 %>%
   mutate(a = case_when(
     c.1.1e.07. < 5000 ~ "inferieur à 5000",
-    TRUE ~ "50000 et +"
+    TRUE ~ "5000 et +"
   )) %>%
   filter(a == "5000 et +") %>%
   mutate(b = case_when(
